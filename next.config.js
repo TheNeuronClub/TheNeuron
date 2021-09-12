f788b6091d0da17d4fb1edbd192e6f2f7bd61579
Ankit Kumar <ankit628792@gmail.com>
2021-09-12 17:09:59 +0530

    'envs'

--- next.config.js
+++ next.config.js
@@ -6,8 +6,9 @@ module.exports = {
         mongodbURI: process.env.MONGODB_URI,
         secret_key: process.env.SECRET_KEY,
         mail_user: process.env.MAIL_USER,
-        pass_user: process.env.PASS_USER,
+        mail_pass: process.env.MAIL_PASS,
         mail_to: process.env.MAIL_TO,
+        host:process.env.HOST,
         NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
     }
 }
