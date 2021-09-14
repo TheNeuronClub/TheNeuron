38ce4d3c6fe21e8c174bbd307fee799bc7cc438e
Ankit <ankit628792@gmail.com>
2021-09-14 22:12:18 +0530

    Copy email text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -56,7 +56,7 @@ const [displayCopy, setDisplayCopy] = useState(true)
                         </div>
                     </Link>
                         <div className="footer__social relative" onClick={() => copyToClipboard()}>
-                            {isCopied ? <span>Email Copied</span> : <span className={`copy__email ${!displayCopy && 'hidden'}`}>Copy Email</span>}
+                            {isCopied ? <span>Email Copied</span> : <span className={`copy__email ${!displayCopy && 'opacity-0'}`}>Copy Email</span>}
                             <svg className="" viewBox="0 0 24 24" aria-hidden="true" tabIndex="-1" title="Mail"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path></svg>
                         </div>
                 </ul>
