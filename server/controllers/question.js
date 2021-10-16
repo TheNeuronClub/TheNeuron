0d35c93b2869e380752de0cfbfe8c43e847133d7
Ankit <ankit628792@gmail.com>
2021-10-16 09:28:35 +0530

    Update question.js

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -29,8 +29,8 @@ const getQuestions = async (req, res) => {
 
 const ques = async (req, res) => {
     try {
-        const trending = await Question.find({ qstatus: 'verified', goLive: { $lte: newDate(new Date().toISOString()) } }).sort({ Volume: -1 }).limit(8);
-        const newest = await Question.find({ qstatus: 'verified', goLive: { $lte: newDate(new Date().toISOString()) } }).sort({ _id: -1 }).limit(8);
+        const trending = await Question.find({ qstatus: 'verified' }).sort({ Volume: -1 }).limit(8);
+        const newest = await Question.find({ qstatus: 'verified' }).sort({ _id: -1 }).limit(8);
         res.status(200).send({ trending, newest })
     } catch (error) {
         res.status(400).send({ msg: 'unable to get question' })
