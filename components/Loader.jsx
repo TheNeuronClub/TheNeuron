e4dd723f49ff6241766baac9eeb8c032a14267cf
Ankit Kumar <ankit628792@gmail.com>
2021-12-03 20:07:55 +0530

    loader updated

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -6,7 +6,7 @@ function Loader() {
                 <div id="loader"></div>
             </div> */}
             {/* </div> */}
-            <div className="loader blur-black">
+            <div className="loader blur-black mx-auto">
                 <div className="face face1"><div className="circle"></div></div>
                 <div className="face face2"><div className="circle"></div></div>
                 <div className="face face3"><div className="circle"></div></div>
